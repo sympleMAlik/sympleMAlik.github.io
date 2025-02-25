9228a23990720e0b5bc11c5451dd720a04309181
sympleMAlik <meb4256454@gmail.com>
2025-02-25 19:02:55 +0000

    datashapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -56,9 +56,18 @@ $(document).ready(function () {
     }
   }
 
-  // TODO 3-a: add a function that handles the static display type
+  // TODO 3-a: add a function that handles the static display type 
+  function handleStatic(data) {
+    setBackgroundWithObject(data)
+    animationDetails.displayType = 1
+    }
 
   // TODO 4-a: add a function that handles the good display type
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+    }
+
 
   // TODO 5-a: add a function that handles the bad display type
 
@@ -68,15 +77,20 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
+    handleStatic(dataShapes[currentIndex]);
+  
   }
 
-  function goodDisplay() {
     // TODO 4-b: call your handleGood function
-  }
+    function goodDisplay() {
+      var currentShape = dataShapes[currentIndex];
+      handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
+      }
+  
 
-  function badDisplay() {
+  
     // TODO 5-b: call your handleBad function
-  }
+  
 
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO ABOVE HERE ////////
@@ -166,10 +180,6 @@ $(document).ready(function () {
   }
   function setBackgroundWithSimple(color, shape, repeat) {
     $("#shape").css("background", `url(images/${color}-${shape}.png)`);
-    setBackgroundRepeat(repeat);
-  }
-  function setBackgroundWithMixed(obj, repeat) {
-    $("#shape").css("background", `url(images/${obj.color}-${obj.shape}.png)`);
     setBackgroundRepeat(repeat);
   }
   function setBackgroundRepeat(repeat) {
